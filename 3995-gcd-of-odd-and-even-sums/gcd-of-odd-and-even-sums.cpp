816ea9377dbdc88d3b6deb56518cd4fd6f0a3c2d
Chirag Bhullar <chiragbhullar07@gmail.com>
2026-07-15 11:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
-    int gcd(int a,int b){
-        if(a==0)
-            return b;
-        if(b==0)
-            return a;
-        if(a==b)
-            return a;
-        if(a>b)
-            return gcd(a-b,b);
-
-        return gcd(a,b-a);
-    }
-    int gcdOfOddEvenSums(int n){
-        if(n==0) return 0;
-        int sumOdd=0,sumEven=0;
-        sumEven=(n*(2+2*n))/2;
-        sumOdd=n*n;
-        int gc=gcd(sumOdd,sumEven);
-        return gc;
+    int gcdOfOddEvenSums(int n) {
+        int summ1 = n*(2*n+1);
+        int eveny = n*(n+1);
+        return __gcd(eveny,summ1-eveny);
     }
 };
